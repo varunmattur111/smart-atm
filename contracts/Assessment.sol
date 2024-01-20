1409ecca90a2e9efff34febc9f7b5bf97a8f11ea
varunmattur111 <154790416+varunmattur111@users.noreply.github.com>
2024-01-20 15:32:42 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,41 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    mapping(address => uint256) public cryptoHoldings;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event BuyBitcoin(address indexed buyer, uint256 amount, uint256 cost);
+    event SellBitcoin(address indexed seller, uint256 amount, uint256 revenue);
 
-    constructor(uint initBalance) payable {
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -47,14 +39,24 @@ contract Assessment {
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function buyBitcoin(uint256 _amount, uint256 _cost) public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(balance >= _cost, "Insufficient balance to buy Bitcoin");
+        cryptoHoldings[msg.sender] += _amount;
+        balance -= _cost;
+        emit BuyBitcoin(msg.sender, _amount, _cost);
+    }
+
+    function sellBitcoin(uint256 _amount, uint256 _revenue) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(cryptoHoldings[msg.sender] >= _amount, "Insufficient Bitcoin holdings to sell");
+        cryptoHoldings[msg.sender] -= _amount;
+        balance += _revenue;
+        emit SellBitcoin(msg.sender, _amount, _revenue);
     }
 }
